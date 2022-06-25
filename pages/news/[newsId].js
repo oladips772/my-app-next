1538d4c8815e379c6aa7578213180410a220e6d7
oladips <oladips200@gmail.com>
2022-06-25 15:00:19 +0000

    added layot to app

--- pages/news/[newsId].js
+++
@@ -1,9 +0,0 @@
-function DetailPage () {
-  return (
-      <div>
-          <h1>Details Page</h1>
-    </div>
-  )
-}
-
-export default DetailPage;
---
+++ pages/posts/FirstPost.js
@@ -0,0 +1,17 @@
+/** @format */
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/Layout";
+
+function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      FirstPost
+    </Layout>
+  );
+} 
+
+export default FirstPost;
